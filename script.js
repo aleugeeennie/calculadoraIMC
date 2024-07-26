01208a3456f3713ef2e9ca2bd467a328aa1f5004
aleugeeennie <aleugeeennie@gmail.com>
2024-07-26 00:34:36 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,48 @@
+/*JS*/
+const formIMC = document.getElementById("formIMC");
+
+//Creamos el párrafo donde va a ir el resultado 
+const row = document.querySelector(".row"); 
+const resultado = document.createElement("div"); 
+const parrafo = document.createElement("p"); 
+resultado.classList.add("resultado"); 
+row.appendChild(resultado);
+resultado.appendChild(parrafo); 
+
+
+//Llamar al evento
+formIMC.addEventListener("submit", obtenerIMC); 
+
+//Función obtener IMC
+function obtenerIMC(e){
+    e.preventDefault(); 
+    //Llamamos los inputs desde el html 
+    const inptPeso = document.getElementById("inptPeso"); 
+    const inptAltura = document.getElementById("inptAltura");
+
+    //Guardamos los valores de los inputs en variables 
+    let peso = Number(inptPeso.value); 
+    let altura = Number(inptAltura.value); 
+
+    if(peso == "" || altura  == "" ){
+        alert("No puede enviar el formulario vacìo"); 
+    } else {
+        operacion(altura, peso); 
+       inptPeso.value = "" 
+       inptAltura.value = ""; 
+    }
+
+}
+
+
+function operacion(altura, peso){
+      //Convierte la altura en metros
+      const convertirMetros = altura/100; 
+      // Eleva la altura al cuadrado
+          const alturaCuadrado = Math.pow(convertirMetros, 2); 
+      //Obtener el IMC
+          const IMC = peso/alturaCuadrado; 
+          resultado.style.display = "block";
+          parrafo.textContent = `Peso: ${peso}. Altura: ${convertirMetros} m². IMC: ${IMC.toFixed(2)};` 
+          return parrafo;
+}
